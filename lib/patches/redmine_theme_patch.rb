e5353294fa7167012c0cc9da781d7d491c14b38b
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2021-06-17 15:25:52 -0300

    Removes debugging messages

--- lib/patches/redmine_theme_patch.rb
+++ lib/patches/redmine_theme_patch.rb
@@ -3,13 +3,9 @@ module RedmineUserThemePlugin
 		
 		
 		included do
-			Rails.logger.info "============== TESTE FINAL ==============="
 		end
 
 		def current_theme
-			Rails.logger.info "============== plugin do tema TESTE 3 ==============="
-			Rails.logger.info Redmine::Themes.theme(User.current.pref[:ui_theme]).to_s
-
 	        @current_theme = Redmine::Themes.theme(User.current.pref[:ui_theme])
 	        @current_theme
       	end
